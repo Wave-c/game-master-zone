2bb3cd7353e97bd59e0d047fcbc67f01027248dd
Wave-c <torrent_den2018@mail.ru>
2024-04-11 20:59:26 +0300

    init project

---
+++ src/main/java/com/wave/gamemasterzone/GameMasterZoneApplication.java
@@ -0,0 +1,13 @@
+package com.wave.gamemasterzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameMasterZoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameMasterZoneApplication.class, args);
+	}
+
+}
